4fcbe5c5f073b96e74850ede2a93239cf9f99104
Jake Butler <JakeButler.student@careerdevs.com>
2022-09-15 19:09:31 -0400

    Initial commit

---
+++ src/day0/Solution.java
@@ -0,0 +1,15 @@
+package day0;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner keyboard = new Scanner(System.in);
+
+        String str = keyboard.nextLine();
+
+        System.out.println("Hello, World." + "\n" + str);
+    }
+}
---
+++ src/day1/Solution.java
@@ -0,0 +1,24 @@
+package day1;
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int a = 4;
+        double b = 4.0;
+        String c = "HackerRank ";
+
+        Scanner keyboard = new Scanner(System.in);
+
+        int i = keyboard.nextInt();
+        double d = keyboard.nextDouble();
+        String blah = keyboard.nextLine();
+        String s = keyboard.nextLine();
+
+        System.out.println(a + i);
+        System.out.println(b + d);
+        System.out.println(c + s);
+    }
+}
