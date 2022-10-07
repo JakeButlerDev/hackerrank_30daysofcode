aaf5922f65c6fd2d1149967ef733ac376e6b3124
Jake Butler <JakeButler.student@careerdevs.com>
2022-10-07 13:35:43 -0400

    Completed two exercises

---
+++ src/Inheritance/Solution.java
@@ -0,0 +1,90 @@
+package Inheritance;
+
+import java.util.*;
+
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
+
+    // Constructor
+    Person(String firstName, String lastName, int identification){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+
+    // Print person data
+    public void printPerson(){
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + 	"\nID: " + idNumber);
+    }
+
+}
+
+class Student extends Person{
+    private int[] testScores;
+
+    /*
+     *   Class Constructor
+     *
+     *   @param firstName - A string denoting the Person's first name.
+     *   @param lastName - A string denoting the Person's last name.
+     *   @param id - An integer denoting the Person's ID number.
+     *   @param scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your constructor here
+    Student(String firstName, String lastName, int idNumber, int[] testScores) {
+        super(firstName, lastName, idNumber);
+        this.testScores = testScores;
+
+    }
+    /*
+     *   Method Name: calculate
+     *   @return A character denoting the grade.
+     */
+    // Write your method here
+    char calculate() {
+        char averageGrade = ' ';
+        int averageNumGrade = 0;
+        int sum = 0;
+        for (int i = 0; i < this.testScores.length; i++) {
+            sum += testScores[i];
+            averageNumGrade = sum/(i+1);
+        }
+        if (averageNumGrade >= 90 && averageNumGrade <= 100) {
+            averageGrade = 'O';
+        } else if (averageNumGrade >= 80 && averageNumGrade < 90) {
+            averageGrade = 'E';
+        } else if (averageNumGrade >= 70 && averageNumGrade < 80) {
+            averageGrade = 'A';
+        } else if (averageNumGrade >= 55 && averageNumGrade < 70) {
+            averageGrade = 'P';
+        } else if (averageNumGrade >= 40 && averageNumGrade < 55) {
+            averageGrade = 'D';
+        } else {
+            averageGrade = 'T';
+        }
+        return averageGrade;
+    }
+}
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String firstName = scan.next();
+        String lastName = scan.next();
+        int id = scan.nextInt();
+        int numScores = scan.nextInt();
+        int[] testScores = new int[numScores];
+        for(int i = 0; i < numScores; i++){
+            testScores[i] = scan.nextInt();
+        }
+        scan.close();
+
+        Student s = new Student(firstName, lastName, id, testScores);
+        s.printPerson();
+        System.out.println("Grade: " + s.calculate() );
+    }
+}
